3e24853793b67bcd87f8cc9b8677c7aff36c7357
elda-o <114947228+elda-o@users.noreply.github.com>
2023-09-24 21:23:32 -0500

    NAVMESH!

--- Assets/Scripts/Enemies/EnemyPathFinding.cs
+++ Assets/Scripts/Enemies/EnemyPathFinding.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NavMeshPlus.Components;
-
+using UnityEngine.AI;
 public class EnemyPathFinding : MonoBehaviour
 {
+
+    private Vector3 target;
+    public GameObject player;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetTargetPoisiton();
+        SetAgentPosition();
         
+    }
+
+    private void SetTargetPoisiton()
+    {
+        target = player.transform.position;
+    }
+
+    private void SetAgentPosition()
+    {
+        agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
     }
 }
