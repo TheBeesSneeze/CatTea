b86e9e6d848bf4fab054dc19cdeb722cee7fb0b2
avandeberg12 <avandeberg@mail.bradley.edu>
2023-10-06 15:49:51 -0500

    Bomber enemy working

--- Assets/Scripts/Enemies/BombEnemyBehaviour.cs
+++ Assets/Scripts/Enemies/BombEnemyBehaviour.cs
@@ -6,13 +6,17 @@ public class BombEnemyBehaviour : EnemyBehaviour
 {
     private GameObject player;
     public GameObject bomb;
+    public GameObject explosion;
     public int amountOfBombs;
     public int bombSpawnInterval;
+    private List<GameObject> listOfBombs = new List<GameObject>();
+    private List<GameObject> listOfExplosions = new List<GameObject>();
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        player = GameObject.FindObjectOfType<PlayerBehaviour>().gameObject;
         StartCoroutine(SpawnBombs());
     }
 
@@ -26,12 +30,31 @@ public class BombEnemyBehaviour : EnemyBehaviour
 
             for (int i = 0; i < amountOfBombs; i++)
             {
-                player = GameObject.FindGameObjectWithTag("Player");
-                Vector3 positionAroundPlayer = Random.insideUnitCircle * player.transform.position;
-                Instantiate(bomb, positionAroundPlayer, Quaternion.identity);
+                Vector3 positionAroundPlayer = player.transform.position;
+                Vector3 randomPosition = Random.insideUnitCircle;
+                positionAroundPlayer.x += randomPosition.x;
+                positionAroundPlayer.y += randomPosition.y;
+                listOfBombs.Add(Instantiate(bomb, positionAroundPlayer, Quaternion.identity));
             }
+            StartCoroutine(Explode());
         }
     }
 
+    private IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(2);
+        for(int i = 0; i < amountOfBombs; i++)
+        {
+            listOfExplosions.Add(Instantiate(explosion, listOfBombs[i].transform.position, Quaternion.identity));
+            Destroy(listOfBombs[i]);
+        }
+        yield return new WaitForSeconds(0.5f);
+        for(int i = 0; i < amountOfBombs; i++)
+        {
+            Destroy(listOfExplosions[i]);
+        }
+        listOfBombs.Clear();
+        listOfExplosions.Clear();
+    }
     
 }
