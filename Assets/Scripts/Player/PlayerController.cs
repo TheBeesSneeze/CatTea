f2aad26dd802459f2db66d83f76afaea2f58be15
BlazingBlitzi <114948273+BlazingBlitzi@users.noreply.github.com>
2023-10-11 12:03:29 -0500

    Bingus'sArt&SoundStuff

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,481 @@
+/*******************************************************************************
+* File Name :         playerController.cs
+* Author(s) :         Toby Schamberger
+* Creation Date :     9/4/2023
+*
+* "Brief" Description : Base class for the player controller scripts to inherit from.
+* Specifically deals with controls.
+* Moving is slightly slippery, slippiness can be changed in code (make the variables
+* public if it bothers you)
+* 
+* Controls Include: Movement, Dashing, Pause.
+*
+* TODO:
+* Other input settings
+* PAUSE
+* player invincible while dashing
+*****************************************************************************/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Unity")]
+    public SpriteRenderer GunSprite;
+    public SpriteRenderer SwordSprite;
+
+    //Really boring settings:
+    [Tooltip("The amount of slippiness the player experiences when changing movement directions (THIS VALUE MUST BE BETWEEN 0 and 1)")]
+    private static float slideAmount = 0.75f; // this needs to be a number between 0 and 1. higher number for more slidey
+    private static float slideSeconds = 0.6f;
+    private static float slideIterations = 25;
+    private static float slowAmount = 0.3f; //also a number between 0 and 1
+    private static float slowSeconds = 0.1f;
+
+    //Player input:
+    protected PlayerInput playerInput;
+
+    protected InputAction move;
+    protected InputAction dash;
+    protected InputAction primary;
+    protected InputAction secondary;
+    protected InputAction aim;
+    [HideInInspector] public InputAction Pause;
+    [HideInInspector] public InputAction Select;
+    [HideInInspector] public InputAction SkipText;
+    protected InputAction cheat;
+
+    // components:
+    protected Rigidbody2D myRigidbody;
+    public Gamepad MyGamepad;
+    protected Animator myAnimator;
+    protected GameManager gameManager;
+    public AudioSource walkSound;
+
+    protected PlayerBehaviour playerBehaviour;
+    protected RangedPlayerController rangedPlayerController;
+    protected MeleePlayerController meleePlayerController;
+
+    protected enum WeaponMode { Gun, Sword };
+    protected WeaponMode CurrentWeapon;
+
+    public enum ControllerType {Keyboard, Controller};
+    [HideInInspector] public ControllerType PlayerControllerType;
+
+    // etcetera:
+    [HideInInspector] public Vector2 MoveDirection;
+    [HideInInspector] public Vector2 InputDirection;
+    [HideInInspector] public Vector2 AimingDirection;
+    protected bool moving; //if a movement key is being pressed rn
+    protected Coroutine movingCoroutine; //shared between SlideMovementDirection and SlowMovement intentionally. They shouldnt run at the same time.
+    protected bool canDash=true;
+    [HideInInspector] public bool CanAttack = true;
+
+    private bool readShootingDirection;
+    private Coroutine aimingCoroutine;
+
+    private bool ignoreMove;
+    [HideInInspector]public bool IgnoreAllInputs;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    protected virtual void Start()
+    {
+        //initialize a lot of variables
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+        //Initialize input stuff
+        playerInput = GetComponent<PlayerInput>();
+        MyGamepad = playerInput.GetDevice<Gamepad>();
+        playerInput.currentActionMap.Enable();
+
+        playerBehaviour = GetComponent<PlayerBehaviour>();
+        rangedPlayerController = GetComponent<RangedPlayerController>();
+        meleePlayerController = GetComponent<MeleePlayerController>();
+
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        StartGunMode();
+
+        InitializeControls();
+
+        DetectInputDevice();
+
+        UpdateAimingDirection();
+        StartCoroutine(UpdateAnimation());
+    }
+
+    /// <summary>
+    /// Sorry Start was getting crowded.
+    /// initalizes every control from every player script
+    /// </summary>
+    private void InitializeControls()
+    {
+        move = playerInput.currentActionMap.FindAction("Move");
+        dash = playerInput.currentActionMap.FindAction("Dash");
+        primary = playerInput.currentActionMap.FindAction("Primary Attack");
+        secondary = playerInput.currentActionMap.FindAction("Secondary Attack");
+        Pause = playerInput.currentActionMap.FindAction("Pause");
+        Select = playerInput.currentActionMap.FindAction("Select");
+        SkipText = playerInput.currentActionMap.FindAction("Skip Text");
+        cheat = playerInput.currentActionMap.FindAction("Cheat");
+        aim = playerInput.currentActionMap.FindAction("Aiming");
+
+        move.performed += Move_performed;
+        move.canceled += Move_canceled;
+
+        dash.performed += Dash_started;
+        dash.canceled += Dash_canceled;
+
+        primary.performed += rangedPlayerController.Gun_performed;
+        primary.canceled += rangedPlayerController.Gun_canceled;
+
+        secondary.performed += meleePlayerController.Sword_started;
+        secondary.canceled += meleePlayerController.Sword_canceled;
+
+        Pause.started += Pause_started;
+
+        cheat.started += Cheat_started;
+
+        aim.performed += Aim_Performed;
+
+    }
+
+    private void Aim_Performed(InputAction.CallbackContext obj)
+    {
+        AimingDirection = obj.ReadValue<Vector2>();
+    }
+
+
+    protected void DetectInputDevice()
+    {
+        try { MyGamepad = playerInput.GetDevice<Gamepad>(); }
+        catch { MyGamepad = null; }
+        
+
+        bool isKeyboardAndMouse = false ;
+        if (MyGamepad == null)
+            isKeyboardAndMouse = true;
+        //bool isKeyboardAndMouse = MyGamepad.description.deviceClass.Equals("Keyboard") || MyGamepad.description.deviceClass.Equals("Mouse");
+
+        if (isKeyboardAndMouse)
+            PlayerControllerType = ControllerType.Keyboard;
+        else
+            PlayerControllerType = ControllerType.Controller;
+    }
+
+    /// <summary>
+    /// Kind of averages the players input direction with the last input direction when
+    /// a key is pressed, so it creates a slightly slippery effect.
+    /// </summary>
+    /// <param name="obj"></param>
+    protected virtual void Move_performed(InputAction.CallbackContext obj)
+    {
+
+        if(ignoreMove || IgnoreAllInputs)
+            return;
+
+        //formatting it like this bc we'll need this variable for animation stuff probably
+        if(movingCoroutine != null)
+            StopCoroutine(movingCoroutine);
+
+        if (!moving)
+        {
+            InputDirection = obj.ReadValue<Vector2>();
+            MoveDirection = InputDirection * playerBehaviour.Speed / 2;
+            walkSound.Play();
+        }
+
+        moving = true;
+
+        movingCoroutine = StartCoroutine(SlideMovementDirection(obj));
+    }
+
+    protected virtual void Move_canceled(InputAction.CallbackContext obj)
+    {
+        if (IgnoreAllInputs) return;
+
+        moving = false;
+
+        if(movingCoroutine != null)
+            StopCoroutine(movingCoroutine);
+
+        movingCoroutine = StartCoroutine(SlowMovement());
+        walkSound.Stop();
+    }
+
+    protected virtual void Dash_started(InputAction.CallbackContext obj)
+    {
+        if (IgnoreAllInputs) return;
+
+        if(canDash)
+            StartCoroutine(PerformDash());
+    }
+
+    protected virtual void Dash_canceled(InputAction.CallbackContext obj)
+    {
+        if (IgnoreAllInputs) return;
+
+        //TODO
+    }
+
+    protected virtual void Pause_started(InputAction.CallbackContext obj)
+    {
+        if (IgnoreAllInputs)
+            return;
+    }
+
+    protected virtual void Cheat_started(InputAction.CallbackContext obj)
+    {
+        if (IgnoreAllInputs) return;
+
+        gameManager.CurrentRoom.Cheat();
+    }
+
+    /// <summary>
+    /// Hides sword and brings out gun.
+    /// Does not do any attacking.
+    /// </summary>
+    public void StartGunMode()
+    {
+        GunSprite.enabled = true;
+        SwordSprite.enabled = false;
+    }
+
+    /// <summary>
+    /// Hides gun and brings out sword.
+    /// Does not do any attacking.
+    /// </summary>
+    public void StartSwordMode()
+    {
+        GunSprite.enabled = false;
+        SwordSprite.enabled = true;
+    }
+
+    /// <summary>
+    /// Kind of unnessecary rn, but im cooking, ok
+    /// </summary>
+    public void UpdateAimingDirection()
+    {
+        if (aimingCoroutine != null)
+            StopCoroutine(aimingCoroutine);
+
+        readShootingDirection = true;
+
+        if (PlayerControllerType.Equals(PlayerController.ControllerType.Keyboard))
+        {
+            rangedPlayerController.RangedIcon.SetActive(true);
+            aimingCoroutine = StartCoroutine(UpdateShootingDirectionByKeyboard());
+        }
+
+        if (PlayerControllerType.Equals(PlayerController.ControllerType.Controller))
+        {
+            rangedPlayerController.RangedIcon.SetActive(false);
+ //           aimingCoroutine = StartCoroutine(UpdateShootingDirectionByController());
+        }
+    }
+
+    private IEnumerator UpdateShootingDirectionByKeyboard()
+    {
+        while (readShootingDirection)
+        {
+            Vector2 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            rangedPlayerController.RangedIcon.transform.position = MousePosition;
+
+            //update shootingDirection. normalize MousePosition to be relative to player
+            MousePosition -= (Vector2)transform.position;
+            MousePosition = MousePosition.normalized;
+
+            AimingDirection = MousePosition;
+
+            //rotate awesome style
+            MousePosition = new Vector2(Mathf.Abs(MousePosition.x), MousePosition.y);
+            float angle = Mathf.Atan2(MousePosition.y, MousePosition.x) * Mathf.Rad2Deg;
+            angle = Mathf.Clamp(angle, rangedPlayerController.MaxDownAngle, rangedPlayerController.MaxUpAngle);
+            rangedPlayerController.RotationPivot.transform.localEulerAngles = new Vector3(0, 0, angle);
+
+            yield return null;
+        }
+        aimingCoroutine = null;
+    }
+
+    private IEnumerator UpdateShootingDirectionByController()
+    {
+        while (readShootingDirection)
+        {
+            AimingDirection = InputDirection;
+
+            rangedPlayerController.CorrectGunPosition();
+
+            yield return new WaitForEndOfFrame();
+        }
+        aimingCoroutine = null;
+    }
+
+    /// <summary>
+    /// transitions the players movement velocity by doing cool math stuff.
+    /// then just becomes regular movement
+    /// </summary>
+    protected IEnumerator SlideMovementDirection(InputAction.CallbackContext obj)
+    {
+        InputDirection = obj.ReadValue<Vector2>();
+        Vector2 newMoveDiection = InputDirection;
+
+        //cool slide
+        for (int i = 0; i < slideIterations && moving; i++)
+        {
+            MoveDirection = BlendMovementDirections(MoveDirection, newMoveDiection, slideAmount);
+
+            if(!ignoreMove)
+                myRigidbody.velocity = MoveDirection * playerBehaviour.Speed;
+
+            yield return new WaitForSeconds(slideSeconds/ slideIterations);
+        }
+
+        //may fuck things up:
+        MoveDirection = newMoveDiection;
+
+        //regular movement
+
+        StartCoroutine(RegularMovement());
+        
+    }
+
+
+
+    protected IEnumerator RegularMovement()
+    {
+        while (moving)
+        {
+            if (!ignoreMove)
+            {
+                myRigidbody.velocity = MoveDirection * playerBehaviour.Speed;
+            }
+            yield return null;
+        }
+        movingCoroutine = null;
+    }
+
+    /// <summary>
+    /// combines the new movement value with the old one so its kinda slidey
+    /// </summary>
+    /// <param name="OldMoveDirection">MoveDirection</param>
+    /// <param name="newReadValue">obj.ReadValue<Vector2>()</param>
+    /// <param name="slideWeight">number between 0 and 1. 0 for instant change. 0.999 for very slidey.param>
+    /// <returns>An averaged movement direction</returns>
+    protected Vector2 BlendMovementDirections(Vector2 OldMoveDirection, Vector2 newReadValue, float slideWeight)
+    {
+        Vector2 a = OldMoveDirection * slideWeight;                         // 10 * 0.7 = 7
+        Vector2 b = newReadValue * (1-slideAmount); // 10 * 0.3 = 3
+
+        return a + b;                                                       // 7 + 3 = 10
+    }
+
+    /// <summary>
+    /// runs after player stops moving
+    /// </summary>
+    /// <returns></returns>
+    protected IEnumerator SlowMovement()
+    {
+        for(int i = 0; i<10 && !moving; i++)
+        {
+            //notice this doesnt update MoveDirection
+            myRigidbody.velocity = BlendMovementDirections(MoveDirection, Vector2.zero, slowAmount);
+
+            yield return new WaitForSeconds(slowSeconds / 10);
+        }
+        myRigidbody.velocity = Vector2.zero;
+        movingCoroutine = null;
+    }
+
+    protected virtual IEnumerator PerformDash()
+    {
+        GameEvents.Instance.OnPlayerDash();
+
+        canDash = false;
+        ignoreMove = true;
+
+        StartCoroutine(NoMovementRoutine(playerBehaviour.DashTime));
+
+        if (movingCoroutine != null)
+            StopCoroutine(movingCoroutine);
+
+        myRigidbody.velocity = Vector2.zero;
+        //myRigidbody.AddForce(MoveDirection * playerBehaviour.DashUnits, ForceMode2D.Impulse);
+
+        MoveDirection = InputDirection * playerBehaviour.Speed;
+        myRigidbody.AddForce(MoveDirection * (playerBehaviour.DashUnits / playerBehaviour.DashTime), ForceMode2D.Impulse);
+
+        if (MyGamepad != null)
+        {
+            MyGamepad.SetMotorSpeeds(0.1f, 0.1f);
+        }
+
+        yield return new WaitForSeconds(playerBehaviour.DashRechargeSeconds);
+        canDash = true;
+    }
+
+    /// <summary>
+    /// no movement for dash reasons
+    /// </summary>
+    /// <param name="Seconds">length of dash ig</param>
+    private IEnumerator NoMovementRoutine(float Seconds)
+    {
+        ignoreMove = true;
+        yield return new WaitForSeconds(Seconds);
+        ignoreMove = false;
+
+        //test
+        if (MyGamepad != null)
+        {
+            MyGamepad.SetMotorSpeeds(0f, 0f);
+        }
+
+        if (moving)
+            myRigidbody.velocity = MoveDirection;
+        else
+            movingCoroutine = StartCoroutine(SlowMovement());
+
+    }
+
+    protected IEnumerator UpdateAnimation()
+    {
+        while(true)
+        {
+            myAnimator.SetFloat("XMovement", AimingDirection.x);
+            myAnimator.SetFloat("YMovement", AimingDirection.y);
+
+            rangedPlayerController.CorrectGunPosition();
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void OnDestroy()
+    {
+        StopAllCoroutines();
+
+        move.performed -= Move_performed;
+        move.canceled -= Move_canceled;
+
+        dash.performed -= Dash_started;
+        dash.canceled -= Dash_canceled;
+
+        primary.performed -= rangedPlayerController.Gun_performed;
+        primary.canceled -= rangedPlayerController.Gun_canceled;
+
+        secondary.performed -= meleePlayerController.Sword_started;
+        secondary.canceled -= meleePlayerController.Sword_canceled;
+
+        Pause.started -= Pause_started;
+
+        cheat.started -= Cheat_started;
+    }
+}
--- Assets/Scripts/Player/RangedPlayerController.cs
+++ Assets/Scripts/Player/RangedPlayerController.cs
@@ -15,14 +15,48 @@ using UnityEngine.InputSystem;
 
 public class RangedPlayerController : DefaultPlayerController
 {
+<<<<<<< Updated upstream
     protected override void Start()
+=======
+    [Header("Ranged Settings")]
+    public GameObject BulletPrefab;
+    [Tooltip("Divides the players speed while they're shooting by this #")]
+    public float SlowWhileShootingAmount;
+
+    [Header("Unity stuff")]
+    public Transform MirrorPivot;
+    public Transform RotationPivot;
+    public GameObject RangedIcon;
+    public GameObject Gun;
+    private PlayerAmmoBar AmmoBar;
+    public AudioSource ShootSound;
+
+
+    public int BulletsLeft
+>>>>>>> Stashed changes
     {
         base.Start();
     }
 
     protected override void Primary_performed(InputAction.CallbackContext obj)
     {
+<<<<<<< Updated upstream
         //TODO
+=======
+        playerBehaviour = GetComponent<PlayerBehaviour>();
+        playerController = GetComponent<PlayerController>();
+        meleePlayerController = GetComponent<MeleePlayerController>();
+
+        AmmoBar = GameObject.FindObjectOfType<PlayerAmmoBar>();
+
+        RangedIcon.transform.SetParent(null);
+
+        canShoot = true;
+
+        RechargeAmmo();
+
+        
+>>>>>>> Stashed changes
     }
     protected override void Primary_canceled(InputAction.CallbackContext obj)
     {
@@ -34,6 +68,96 @@ public class RangedPlayerController : DefaultPlayerController
     }
     protected override void Secondary_canceled(InputAction.CallbackContext obj)
     {
+<<<<<<< Updated upstream
         //TODO
+=======
+        float oldSpeed = playerBehaviour.Speed;
+        playerBehaviour.Speed = oldSpeed / 2;
+
+        while (PrimaryShooting && canShoot && BulletsLeft > 0)
+        {
+            BulletsLeft--;
+            ShootBullet();
+            yield return new WaitForSeconds(playerBehaviour.TimeBetweenShots);
+        }
+
+        playerBehaviour.Speed = oldSpeed;
+
+        canShoot = true;
+        RechargeAmmo();
+        shootingCoroutine = null;
+    }
+
+    /// <summary>
+    /// Mirrors the scale awesome style
+    /// </summary>
+    /// <returns></returns>
+    public void CorrectGunPosition()
+    {
+        if (playerController.AimingDirection.x > 0)
+            MirrorPivot.localScale = new Vector3(1, 1, 1);
+
+        if (playerController.AimingDirection.x < 0)
+            MirrorPivot.localScale = new Vector3(-1, 1, 1);
+    }
+    
+
+    /// <summary>
+    /// instantiates a bullet!
+    /// this is gonna be bonkers once upgrades are added, i worry
+    /// </summary>
+    private void ShootBullet()
+    {
+        
+        GameEvents.Instance.OnPlayerShoot();
+
+        //spawn the thing
+        GameObject bullet = Instantiate(BulletPrefab, Gun.transform.position, Quaternion.identity);
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        
+
+
+
+        bulletRigidbody.velocity = playerController.AimingDirection * playerBehaviour.ProjectileSpeed;
+
+        //rotate
+        float Angle = Vector2.SignedAngle(Vector2.right, playerController.AimingDirection);
+        Vector3 TargetRotation = new Vector3(0, 0, Angle);
+        bullet.transform.eulerAngles = TargetRotation;
+        ShootSound.Play();
+    }
+
+    private void SetBulletsLeft(int value)
+    {
+        _bulletsLeft = value;
+
+        if (AmmoBar == null)
+            return;
+
+        AmmoBar.UpdateAmmo();
+    }
+
+    /// <summary>
+    /// (attempts to) start coroutine which reloads ammo over some interval
+    /// </summary>
+    public void RechargeAmmo()
+    {
+        if (reloadAmmoCoroutine != null)
+            return;
+
+        reloadAmmoCoroutine = StartCoroutine(RechargeAmmoCoroutine());
+    }
+
+    private IEnumerator RechargeAmmoCoroutine()
+    {
+        yield return new WaitForSeconds(playerBehaviour.AmmoRechargeTime);
+
+        while (!PrimaryShooting && BulletsLeft < playerBehaviour.ShotsPerBurst)
+        {
+            BulletsLeft++;
+            yield return new WaitForSeconds(playerBehaviour.AmmoRechargeTime);
+        }
+        reloadAmmoCoroutine = null;
+>>>>>>> Stashed changes
     }
 }
