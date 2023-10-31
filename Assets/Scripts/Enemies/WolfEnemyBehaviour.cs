1dd19a498dcfc45e986b590d3746265202463e90
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2023-10-31 17:57:25 -0500

    moved stuff

--- Assets/Scripts/Enemies/WolfEnemyBehaviour.cs
+++ Assets/Scripts/Enemies/WolfEnemyBehaviour.cs
@@ -5,37 +5,21 @@
 *
 * Brief Description : sorry i deleted everything. i made it extend DogEnemyBehaviour
 *****************************************************************************/
+/*******************************************************************************
+* File Name :         WolfEnemyBehaviour.cs
+* Author(s) :         Aiden Vandeberg
+* Creation Date :     
+*
+* Brief Description : 
+*****************************************************************************/
+
 
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class WolfEnemyBehaviour : DogEnemyBehaviour
 {
-    protected Animator wolfAnimator;
-
-    protected Vector2 enemyDirection;
-
-    protected override void Start()
-    {
-        wolfAnimator = GetComponent<Animator>();
-
-        StartCoroutine(UpdateAnimation());
-    }
-    protected IEnumerator UpdateAnimation()
-    {
-        while (true)
-        {
-            enemyDirection.x = GetComponent<NavMeshAgent>().velocity.x;
-            enemyDirection.y = GetComponent<NavMeshAgent>().velocity.y;
-
-            //Debug.Log(enemyDirection);
-
-            wolfAnimator.SetFloat("XMovement", enemyDirection.x);
-            wolfAnimator.SetFloat("YMovement", enemyDirection.y);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
+    
 }
