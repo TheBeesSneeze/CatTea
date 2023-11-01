d8054d239a95194dfdf629c338e4660e4ab85804
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2023-11-01 12:17:02 -0500

    Alpha Build
    THE GAMES PLAYABLE WOOO

--- Assets/Scripts/Rooms/BossRoom.cs
+++ Assets/Scripts/Rooms/BossRoom.cs
@@ -78,5 +78,11 @@ public class BossRoom : EnemyRoom
         return (bossDead);
     }
 
+    public override void Cheat()
+    {
+        if(bossScript != null) 
+            bossScript.Die();
 
+        base.Cheat();
+    }
 }
