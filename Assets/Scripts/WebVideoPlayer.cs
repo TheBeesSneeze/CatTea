dc607ed139cd96a3558b24dda5cc3148fd2177ee
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2023-12-16 15:21:08 -0800

    Fixing Video Stuff

---
+++ Assets/Scripts/WebVideoPlayer.cs
@@ -0,0 +1,28 @@
+/*******************************************************************************
+* File Name :         WebVideoPlayer.cs
+* Author(s) :         Toby Schamberger
+* Creation Date :     12/16/2023
+*
+* Brief Description : why are you reading this. unless youre toby from the future
+* or something
+*****************************************************************************/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class WebVideoPlayer : MonoBehaviour
+{
+    public string VideoName;
+    private VideoPlayer videoPlayer;
+    
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, VideoName+".mp4");
+    }
+}
